789b226738eb7d00399aec0f0f9a5340154b3c5b
JoanLazo <joan162581@gmail.com>
2018-11-20 12:59:10 -0500

    botones con funcion inicio,cifrar y descifrar style none y block

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 
-
-
   const textResult = document.getElementById("respuesta");
   const botonCifrar = document.getElementById("btn-cifrar");
 
   function cifrar(inputString,offsetValue){
     let subjectText;     
     let subjectAscii;
+    // let spaceText= " ";
     let textResultado = "";
 
     for(let i=0; i<inputString.length ;i++){
      subjectText = inputString[i].charCodeAt(); // obtenemos su codigo ASCII
-     subjectAscii = subjectText + offsetValue; // al valor de la letra de sumamos el desplazamiento
-     textResultado = textResultado.concat(String.fromCharCode(subjectAscii)); // se unio el valor ascci con el resultado fromCharCode devolvera el valor en letras
+     subjectAscii = subjectText + offsetValue;
+     textResultado = textResultado.concat(String.fromCharCode(subjectAscii));
     }
     return textResultado;
     }
@@ -35,9 +34,11 @@
         let subjectTextTwo;     
         let subjectAsciiTwo;
         let textResultTwo = "";
+        let capitalLetter;
     
         for(let i=0; i<inputString.length ;i++){
-         subjectTextTwo = inputString[i].charCodeAt(); // obtenemos su codigo ASCII
+         capitalLetter = (i=== i.toUpperCase()) ? true : false;
+         subjectTextTwo = inputString[i].toLowerCase().charCodeAt(); // obtenemos su codigo ASCII
          subjectAsciiTwo = subjectTextTwo - offsetValue; // al valor de la letra de sumamos el desplazamiento
          textResultTwo = textResultTwo.concat(String.fromCharCode(subjectAsciiTwo)); // se unio el valor ascci con el resultado fromCharCode devolvera el valor en letras
         }
@@ -51,3 +52,51 @@
         const offsetValue = document.getElementById("desplazamiento2").value;
         textResultOne.innerHTML = descifrar(inputString,parseInt(offsetValue))
       })
+
+  //ocultando partes de div cifrar y descifrar
+document.getElementById("partecifrar").style.display = "none"
+document.getElementById("partedescifrar").style.display = "none"
+
+//boton cifrar para jugar
+const btnCifrar= document.getElementById("btn-inicio-cifrar");
+
+function jugarCifrar(){
+  const inicioCifrarJugar = document.getElementById("inicio").value; 
+  document.getElementById("partecifrar").style.display = "block"
+  document.getElementById("inicio").style.display = "none"
+}
+
+btnCifrar.addEventListener("click",jugarCifrar);
+
+//boton volver al inicio desde cifrar
+const btnInicio = document.getElementById("btn-volver1");
+
+function volverIncioOne(){
+  const volverIncioCifrar = document.getElementById("partecifrar").value;
+  document.getElementById("inicio").style.display = "block"
+  document.getElementById("partecifrar").style.display = "none"
+}
+btnInicio.addEventListener("click",volverIncioOne);
+
+//boton descifrar jugar
+const btnDescifrar = document.getElementById("btn-inicio-descifrar");
+
+function jugarDescifrar(){
+  const inicioDescifrarJugar = document.getElementById("inicio").value;
+  document.getElementById("partedescifrar").style.display = "block"
+  document.getElementById("inicio").style.display = "none"
+}
+btnDescifrar.addEventListener("click",jugarDescifrar);
+
+
+//boton volver al inicio desde descifrar
+const  btnIncioDescifrar = document.getElementById("btn-volver2");
+function volverDescifrar(){
+  const inicioDescifrarVolver = document.getElementById("partedescifrar").value;
+  document.getElementById("inicio").style.display = "block"
+  document.getElementById("partedescifrar").style.display = "none"
+}
+btnIncioDescifrar.addEventListener("click",volverDescifrar);
+
+
+
